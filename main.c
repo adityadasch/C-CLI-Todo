d6ebf2a93d36d5405570acf9985b4d59eba8c0b8
Aditya CH <adityadasch@gmail.com>
2026-03-05 12:00:23 +0530

    Added working create task and added gitignore

--- main.c
+++ main.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <string.h>
-#include "cJSON.h"
-
+#include "lib/cJSON.h"
+#include <stdlib.h>
 
 void readFromJSON(char *buffer, size_t size){
 	FILE *fp = fopen("task.json", "r");
@@ -25,8 +25,39 @@ void writeToJSON(cJSON *json){
 
 void stringTillEnd(char *src, int offset, char *out){
 	strcpy(out, src+offset);
-	printf("Okay\n");
 }
+
+void createTask(char *input, int *keyNumber, cJSON *json){
+	char task[strlen(input)+3];
+	if (strlen(input) > 4) { // The input is more than "new "
+			stringTillEnd(input, 4, task); // Remove the task trailing space
+	}
+	char key[125];
+	sprintf(key,"%d",(*keyNumber));
+	cJSON *array = cJSON_CreateArray();
+	cJSON_AddItemToArray(array, cJSON_CreateFalse());
+	cJSON_AddItemToArray(array, cJSON_CreateString(task));
+
+	cJSON_AddItemToObject(json, key, array);
+	(*keyNumber)++;
+}
+
+int readLineFromStdin(char *buf, size_t size) {
+	printf(">");
+    if (fgets(buf, size, stdin) == NULL) {
+        return 1; // error or EOF
+    }
+    buf[strcspn(buf, "\n")] = '\0'; // trim newline
+
+    // flush leftovers if truncated
+    if (strlen(buf) == size - 1 && buf[size - 2] != '\n') {
+        int c;
+        while ((c = getchar()) != '\n' && c != EOF) { }
+    }
+    return 0;
+}
+
+
 
 int main(){
 	char buffer[1024];
@@ -39,29 +70,18 @@ int main(){
 	int keyNumber = cJSON_GetObjectItemCaseSensitive(json, "keyNumber")->valueint;
 	bool flag = true;
 	char input[255];
+
 	while(flag){
-		memset(input,0 ,sizeof(input));	
-		scanf("%40s", input);
+		if(readLineFromStdin(input, sizeof(input))==1){
+			continue;
+		}
 		if(strlen(input)==1 // Only one character
 		   && input[0] == 'q'){
 			flag = false;
 			break;
+		} else if(strncmp(input, "new", 3) == 0){
+			createTask(input, &keyNumber, json);
 		}
-		if(strncmp(input, "new", 3) != 0){
-			printf("Works\n");
-			char task[255];
-			
-			if (strlen(input) > 3) {
-    				stringTillEnd(input, 3, task);
-			}
-
-			printf("%s", task);
-			char key[125];
-			sprintf(key,"%d",keyNumber),
-			cJSON_AddItemToObject(json, key, cJSON_CreateString(task));
-			keyNumber++;
-		}
-	}
 	cJSON_ReplaceItemInObjectCaseSensitive(json, "keyNumber",  cJSON_CreateNumber(keyNumber));
 	writeToJSON(json);
 	return 0;
